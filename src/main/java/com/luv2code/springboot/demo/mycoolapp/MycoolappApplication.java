13fd5e5da22b141cb21539fcbfb0f079ba7bf2e6
sourabh9580 <96277134+sourabh9580@users.noreply.github.com>
2023-05-03 12:38:51 +0530

    Update MycoolappApplication.java

--- src/main/java/com/luv2code/springboot/demo/mycoolapp/MycoolappApplication.java
+++ src/main/java/com/luv2code/springboot/demo/mycoolapp/MycoolappApplication.java
@@ -2,12 +2,19 @@ package com.luv2code.springboot.demo.mycoolapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class MycoolappApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MycoolappApplication.class, args);
 	}
+	
+	@RequestMapping("/")
+   	String helloWorld() {
+        return "5. please visit my website learning-ocean.com";
 
 }
