f01949575d3b763198e5282071725c1ee1dc3e00
sourabh9580 <96277134+sourabh9580@users.noreply.github.com>
2023-05-03 12:24:40 +0530

    Update FunRestController.java

--- src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.java
+++ src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -2,7 +2,7 @@ package com.luv2code.springboot.demo.mycoolapp.rest;
 
 import java.time.LocalDateTime;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
